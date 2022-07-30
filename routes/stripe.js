c09538fc05057d5b628948857e52f6cb0938468b
Pallavi <pallavi.ugam@gmail.com>
2022-07-30 12:28:19 +0530

    make it better

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,3 +1,35 @@
+// const express = require('express')
+// const Stripe = require('stripe')
+// require('dotenv').config()
+
+// const stripe = Stripe(process.env.STRIPE_KEY)
+
+// const router = express.Router()
+
+// router.post("/create-checkout-session", async (req, res) => {
+//     const session = await stripe.checkout.sessions.create({
+//         line_items: [{
+//             price_data: {
+//               currency: 'inr',
+//               product_data: {
+//                 name: 'T-shirt',
+//               },
+//               unit_amount: 3000,
+//             },
+//             quantity: 4,
+//           }],
+//           mode: 'payment',
+
+//           success_url: 'https://ecommerceappsite.herokuapp.com/checkout-success',
+//           cancel_url: 'https://ecommerceappsite.herokuapp.com/cart',
+//         })
+      
+//         res.send({url: session.url})
+
+//   })
+
+// module.exports = router
+
 const express = require('express')
 const Stripe = require('stripe')
 require('dotenv').config()
@@ -7,17 +39,40 @@ const stripe = Stripe(process.env.STRIPE_KEY)
 const router = express.Router()
 
 router.post("/create-checkout-session", async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [{
-            price_data: {
-              currency: 'inr',
-              product_data: {
-                name: 'T-shirt',
-              },
-              unit_amount: 3000,
+
+  const line_items = req.body.cartItems.map((item) => {
+    return{
+      // price_data: {
+        //   currency: 'inr',
+        //   product_data: {
+          //     name: item.title,
+          //     // images: [item.images],
+          //     description: item.description ,
+              
+          //   },
+          //   unit_amount: 120 * 100,
+          // },
+          // quantity: item.quantity,
+          
+          price_data: {
+            currency: 'inr',
+            product_data: {
+              name: item.title,
+              // Images: [item.images],
+              description: item.description,
+              metadata: {
+                id: item._id
+              }
             },
-            quantity: 4,
-          }],
+            unit_amount: item.price * 100,
+          },
+          quantity: item.quantity,
+        }
+      })
+      
+      // console.log([item.images])
+      const session = await stripe.checkout.sessions.create({
+        line_items,
           mode: 'payment',
 
           success_url: 'https://ecommerceappsite.herokuapp.com/checkout-success',
